939f3132e73d520e1875236153c6452e8377ba3b
mati <mati@DESKTOP-GAKM3OF>
2021-04-21 13:26:00 +0200

    commit 1

---
+++ Exemple4/src/Test/test.java
@@ -0,0 +1,13 @@
+package Test;
+
+import java.util.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner (System.in);
+
+	}
+
+}
